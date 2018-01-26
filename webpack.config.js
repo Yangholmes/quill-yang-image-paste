a3d2c1ba84f48e781e5c2fbafacc4ad5e3c38dbe
Yangholmes <yangholmes@126.com>
2018-01-26 11:51:27 +0800

    [modified]update version

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@
  * @file
  * @author Yangholmes
  */
-'use strict';
-const webpack = require('webpack');
 const path = require('path');
 
 const config = {
@@ -12,19 +10,19 @@ const config = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }
-        }]
     }
+    // module: {
+        // rules: [{
+        //     test: /\.js$/,
+        //     exclude: /(node_modules|bower_components)/,
+        //     use: {
+        //         loader: 'babel-loader',
+        //         options: {
+        //             presets: ['@babel/preset-env']
+        //         }
+        //     }
+        // }]
+    // }
     // plugins: [
     //     new webpack.optimize.UglifyJsPlugin({
     //         compress: {
